ad282e580a3a8afbcec700e6afa42d17e9e0af91
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-04 16:03:02 -0800

    Point proven.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,30 @@
 ﻿namespace dictionaries;
 
-struct Product
+struct Product : IComparable<Product>
 {
     public int ProductId;
     public string VendorName;
 
     public override string ToString() => $"ProductId: {ProductId}, VendorName: {VendorName}";
+
+    public int CompareTo(Product other)
+    {
+        if (ProductId < other.ProductId)
+        {
+            return -1;
+        }
+        if (ProductId > other.ProductId)
+        {
+            return 1;
+        }
+        return VendorName.CompareTo(other.VendorName);
+    }
 }
 
 class Program
 {
+    public Random random = new Random();
+
     static void PrintPrices(Dictionary<Product, decimal> prices)
     {
         foreach (var price in prices)
@@ -19,6 +34,41 @@ class Program
     }
 
     static void Main(string[] args)
+    {
+        var program = new Program();
+        program.FillDictionaries();   
+    }
+
+    void FillDictionaries()
+    {
+        var prices = new Dictionary<Product, decimal>();
+        FillPrices(prices);
+        var prices2 = new SortedDictionary<Product, decimal>();
+        FillPrices(prices2);
+    }
+
+    string RandomName()
+    {
+        return new string(Enumerable.Range(0, 8).Select(i => (char)('A' + random.Next(0, 26))).ToArray());
+    }
+
+    void FillPrices(IDictionary<Product, decimal> prices)
+    {
+        var memory = GC.GetTotalMemory(true) / 1024;
+        Console.WriteLine("memory: {0}", memory);
+        for (int i = 0; i < 100000; i++)
+        {
+            prices.Add(new Product { ProductId = i, VendorName = RandomName() }, i);
+        }
+        memory = GC.GetTotalMemory(true) / 1024;
+        Console.WriteLine("memory: {0}", memory);
+        prices.Clear();
+        GC.Collect();
+        memory = GC.GetTotalMemory(true) / 1024;
+        Console.WriteLine("memory: {0}", memory);
+    }
+
+    static void CompareDictionaries()
     {
         var prices = new Dictionary<Product, decimal> {
             { new Product { ProductId = 1, VendorName = "Contoso" }, 100m },
